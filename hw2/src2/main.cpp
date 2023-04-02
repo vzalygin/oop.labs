989b53d5f14f6bb324a150cee873180d2bc6bdc4
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2023-04-02 22:40:00 +0300

    hw2

--- hw2/src2/main.cpp
+++ hw2/src2/main.cpp
@@ -61,6 +61,5 @@ int main() {
         }
     }
     
-
     return 0;
 }
---
+++ hw2/src3/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+int main() {
+    FILE *f;
+    f = fopen("./f", "w");
+    return 0;
+}
